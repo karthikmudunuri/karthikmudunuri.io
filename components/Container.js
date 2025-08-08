e06769f6a7734adfb4ecc7ddf58dc42afde6e266
Mudunuri bhaskara karthikeya varma <102793643+karthikmudunuri@users.noreply.github.com>
2025-08-08 10:45:32 +0530

    new update

--- components/Container.js
+++ components/Container.js
@@ -90,15 +90,10 @@ export default function Container(props) {
         />
         <link rel="canonical" href={`https://karthikmudunuri.io${router.asPath}`} />
         <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content="Manu Arora" />
+        <meta property="og:site_name" content="Karthik Mudunuri" />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@mannupaaji" />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
         {meta.date && (
           <meta property="article:published_time" content={meta.date} />
         )}
